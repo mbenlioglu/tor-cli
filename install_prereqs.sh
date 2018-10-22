7a822ddd8ce6e794d8e233be21d2e29ad7049ece
M. Mucahid Benlioglu <m.mucahidbenlioglu@gmail.com>
2018-10-22 16:24:42 +0300

    - Removed previously added binaries (not necessery) + Added prerequisite installation script. + Added template file for gpg unattended key generation

---
+++ install_prereqs.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+if [ $EUID != 0 ]; then
+    sudo "$0" "$@"
+    exit $?
+fi
+
+TOR_CLI_HOME='.torcli'
+
+BROWN='\033[0;33m'
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+# Distro check
+if [ "$(expr substr $(uname -s) 1 5)" != "Linux" ]; then
+    echo -e "${RED}This script currently works on Linux only. Exiting${NC}"
+    exit 1
+fi
+
+# Not used now, not deleted as it may be needed for later updates
+get_latest_release() {
+  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
+    grep '"tag_name":' |                                            # Get tag line
+    sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
+}
+
+echo -e "${GREEN}Fetching prerequired packages${NC}"
+
+mkdir -p ~/$TOR_CLI_HOME/bin/
+echo -e "${BROWN}"
+# Download gdrive
+echo 'Installing gdrive'
+if [ $(uname -m) = "i386" ]; then
+    dwnlink="https://docs.google.com/uc?id=0B3X9GlR6EmbnLV92dHBpTkFhTEU&export=download"
+elif [ $(uname -m) = "x86_64" ]; then
+    dwnlink="https://docs.google.com/uc?id=0B3X9GlR6EmbnQ0FtZmJJUXEyRTA&export=download"
+fi
+curl -L $dwnlink -o ~/$TOR_CLI_HOME/bin/gdrive --progress-bar
+chmod +x ~/$TOR_CLI_HOME/bin/gdrive
+
+#Install deluge, gnupg, pigz
+if [ "$1" -eq "remote" ]; then
+    echo y | apt-get install pigz deluged deluge-console gnupg
+    echo "{
+  \"file\": 1, 
+  \"format\": 1
+}{
+  \"commands\": [
+    [
+      \"0\", 
+      \"added\", 
+      \"$(pwd)/onTorrentAdded.sh\"
+    ], 
+    [
+      \"1\", 
+      \"complete\", 
+      \"$(pwd)/onTorrentComplete.sh\"
+    ]
+  ]
+}" > ~/.config/deluge/execute.conf
+    deluged && deluge-console plugin -e Execute
+elif [ "$!" -eq "local" ]; then
+    echo y | apt-get install gnupg
+else
+    echo -e "${RED} wrong parameter${NC}"
+	exit $?
+fi
+
+echo -e "${GREEN}Done.${NC}"
